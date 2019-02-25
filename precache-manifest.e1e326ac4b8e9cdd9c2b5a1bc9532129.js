6908d7437a9622d75ba297182a6f23387c38ae3e
Ava Guerrero <ava.square@gmail.com>
2019-02-26 01:05:42 +0800

    Updates

---
+++ precache-manifest.e1e326ac4b8e9cdd9c2b5a1bc9532129.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6298c849f8084a5169ae237be5eee80e",
+    "url": "/the-tv-database/static/media/preloader.6298c849.svg"
+  },
+  {
+    "revision": "838347e14be14d2c50f208a8ddc9938b",
+    "url": "/the-tv-database/static/media/icon_heart.838347e1.png"
+  },
+  {
+    "revision": "9b3fe2d5934da3f0c09f",
+    "url": "/the-tv-database/static/js/runtime~main.9b3fe2d5.js"
+  },
+  {
+    "revision": "ae421ebc27dc236686d6",
+    "url": "/the-tv-database/static/js/main.ae421ebc.chunk.js"
+  },
+  {
+    "revision": "fed534b60cfbd0842d9c",
+    "url": "/the-tv-database/static/js/2.fed534b6.chunk.js"
+  },
+  {
+    "revision": "ae421ebc27dc236686d6",
+    "url": "/the-tv-database/static/css/main.dcd3d0a3.chunk.css"
+  },
+  {
+    "revision": "fed534b60cfbd0842d9c",
+    "url": "/the-tv-database/static/css/2.8cb33725.chunk.css"
+  },
+  {
+    "revision": "62bcb76dbf6dae5c43456a4083e39206",
+    "url": "/the-tv-database/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/the-tv-database/precache-manifest.e1e326ac4b8e9cdd9c2b5a1bc9532129.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/the-tv-database/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
