9c261a2d794870c1f8ab0233dc1dce1c62c64252
Ava Guerrero <ava.square@gmail.com>
2019-02-25 22:35:07 +0800

    feat: Import bootstrap css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
